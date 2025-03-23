aee05b08e3ddf8b29f16ddbec42e297f63364f89
祥云洛浦 <2289850753@qq.com>
2025-03-23 23:03:28 +0800

    88

---
+++ src/main/java/test_2025_3_23/Solution.java
@@ -0,0 +1,20 @@
+package test_2025_3_23;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4, 4, 9, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{3, 3, 6};
+        int n = 3;
+        int p = m + n - 1;
+        m -= 1;
+        n -= 1;
+        while (m > -1 && n > -1)
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        while (n > -1) nums1[p--] = nums2[n--];
+        System.out.println(Arrays.toString(nums1));
+    }
+}
